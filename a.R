0c84c21fcebca00d4f10a7260c76355b8298099a
yyyaaan <yan@yan.fi>
2018-08-10 13:34:02 +0000

    v1.3 system chip pie

--- a.R
+++ a.R
@@ -1,21 +1,49 @@
 library(tidyverse); library(plotly)
+data <- readRDS("dataOK.rds")
 myplot <- function(vector) plot_ly(data) %>% add_markers(x = 1:length(vector), y = vector)
 
 
-# plotting ----------------------------------------------------------------
-
-ggplot(data, aes(x = the.release.date, y = the.total.pixels, color = the.total.pixels.group)) + geom_jitter() + geom_smooth()
-ggplot(data, aes(x = the.release.date, y = the.total.pixels.group)) + geom_jitter() + geom_smooth()
-ggplot(data, aes(x = the.release.date, y = the.screen2body.percent)) + geom_jitter() + geom_smooth()
+# simple plots ------------------------------------------------------------
 
 
-# processors --------------------------------------------------------------
+ggplot(data, aes(x = the.release.date, y = the.total.pixels.group)) + 
+  geom_jitter() + geom_smooth()
 
-data %>% filter(!is.na(System.chip)) %>% with(System.chip) %>% word()
+ggplot(data, aes(x = the.release.date, y = the.screen2body.percent)) + 
+  geom_jitter() + geom_smooth()
 
+
+# complicated plots -------------------------------------------------------
+
+options(stringsAsFactors = F)
+temp <- summary(na.omit(data$the.systemchip.brand)) / length(na.omit(data$the.systemchip.brand))
+temp <- data.frame(brand = names(temp), value = temp)
+temp <- temp[order(temp$value),]
+temp <- rbind(temp, c("others", sum(temp$value[1:14])))
+temp <- temp[15:nrow(temp),]
+ggplot(temp, aes("System Chip", y = as.numeric(value), fill = brand)) +
+  geom_col(width = 1) + coord_polar(theta = "y")
+remove(temp)
 
 # processed data ----------------------------------------------------------
 
+cleanSystemChip <- function(){
+  data$the.systemchip.brand <- data$System.chip %>% word()
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "")] <- NA
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "Broadcomm")] <- "Broadcom"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "Qulacomm")] <- "Qualcomm"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "Xiaolong")] <- "Qualcomm"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "RDA8810")] <- "RDA"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "RDA8810PL")] <- "RDA"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "MT6276a")] <- "MediaTek"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "Hi-Silicon")] <- "HiSilicon"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "Nvidia")] <- "NVIDIA"
+  data$the.systemchip.brand[which(data$the.systemchip.brand == "STE")] <- "ST-Ericsson"
+  # factorize
+  data$the.systemchip.brand <- as.factor(data$the.systemchip.brand)
+  # see it
+  data$the.systemchip.brand %>% unique() %>% sort()
+}
 
 sceenSpecs <- function(){
   
